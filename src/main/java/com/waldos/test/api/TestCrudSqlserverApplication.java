ae95dc1581b2c0a035bef845f4219e6d9a46e56d
RubenDelgado <rdelgado.stc@gmail.com>
2021-04-22 17:43:50 -0500

    Primer commit

---
+++ src/main/java/com/waldos/test/api/TestCrudSqlserverApplication.java
@@ -0,0 +1,13 @@
+package com.waldos.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCrudSqlserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCrudSqlserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waldos/test/api/TestCrudSqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldos.test.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCrudSqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
